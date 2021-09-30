2ed9ead1ed609690e6e750d2fe2e9647904b35db
Pravin M Singh <57350248+Pravinakajerry@users.noreply.github.com>
2021-09-30 12:19:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("time").innerHTML = "EXPIRED";
+    document.getElementById("time").innerHTML = "Coming Soon";
   }
 }, 1000);
